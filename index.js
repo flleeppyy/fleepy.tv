c7a286859567c4a7dc89c8eda61e4b27ecbe2aab
Flleeppyy <flleeppyy@gmail.com>
2020-05-21 00:47:07 -0700

    Add snow.css; Move <script> element contents to all.js

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const path = require("path")
 
 const express = require("express")
 const app = express() // let app be express()
+const cheerio = require('cheerio');
+const fs = require('fs')
 
 const port = 8001
+
 
 app.use('/other_stuff', express.static(__dirname + '/public/other_stuff')) // Static route
 app.use('/assets', express.static(__dirname + '/public/assets')) // Static route, /src serves anything in the public/assets folder
 
 app.get('/favicon.ico', (req, res) => res.sendFile(path.join(__dirname, 'public/favicon.ico')))
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // The index page
     console.log(`IP: ${req.ip} Requested ${req.url}`) // just do some logging
     res.sendFile(
             path.join(__dirname, 'public/index.html')
@@ -25,5 +28,17 @@ app.get('/', (req, res) => {
 
 });
 
+// Other files and shit
+
+app.get('/cirnobot/strings.json', (req, res) => {
+    var html = fs.readFileSync('/mnt/botimagestorage/cirnobot/strings.json', 'utf8');
+    var $ = cheerio.load(html);
+    var styleNode = '<style>body { white-space: pre }</script>';
+    var contribution = '<p>Submit Cirno quotes <a href="https://u.fleepy.tv/cirnobotquotesubmission">here!</a></p>'
+    $('body').prepend(contribution);
+    $('body').append(styleNode);
+    //res.setHeader('Content-Type', 'application/json');
+    res.send($.html());
+})
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`)) // listen the server, make it live
