ee0b17734f521ea035782cf39db11e25644da4ae
Flleeppyy <fleeppyybusiness@gmail.com>
2022-02-03 11:01:27 -0800

    - fix dev

--- frontend_fancy/src/ts/modules/dev.ts
+++ frontend_fancy/src/ts/modules/dev.ts
@@ -1,36 +1,54 @@
-import io from "socket.io-client";
-
 export default async (): Promise<void> => {
-  const devFetch = parseInt(await (await fetch("/dev")).text()); 
-  if (devFetch) { 
-    window.dev = true;
-    console.info("INFO | Dev enabled")
-    const socketLocation = `http://${window.location.hostname}:8081`
-    const socket = io(socketLocation);
-    const style = $("<style></style>").appendTo("head");
-    console.info("INFO | Socket location: " + socketLocation)
-
-    let fetchCss = async () => {
-      // $.get("/css/main.css", (data) => {
-      //   style.html(data);
-      // });
-      const data = await (await fetch("/css/main.css")).text();
-      style.html(data)
+  const checkDev = (await fetch("/dev")).text()
+  
+  if (checkDev) {
+    let cssBlob = await (await fetch("/css/styles.css")).blob();
+    const stylesheet: HTMLLinkElement = document.querySelector("link[id='stl']");
+    stylesheet.href = URL.createObjectURL(cssBlob);
+    let socket;
+    let socketRetrys = 0;
+    let maxSocketRetrys = 10;
+    // @ts-expect-error
+    function initSocket() {
+      socket = new WebSocket("ws://127.0.0.1:8081");
+      let open = false;
+      
+      socket.onerror = function(ev) {
+        console.error(ev)
+      }
+      
+      socket.onmessage = async function(ev) {
+        if (!open && ev.data == "hihi") {
+          open = true;
+          console.info("Socket connected");
+          return;
+        }
+        try {
+          const data = JSON.parse(ev.data);
+          if (data.action === "refreshPage") {
+            console.info("Refreshing page");
+            window.location.reload();
+          } else if (data.action === "refreshCss") {
+            console.info("Refreshing css");
+            stylesheet.href = window.URL.createObjectURL(await (await fetch("css/main.css")).blob());
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      }
+      socket.onclose = function(ev) {
+        // if the page is reloading
+        if (ev.code == 1001) {
+          return;
+        }
+        if (socketRetrys > maxSocketRetrys) {
+          console.error("Socket closed. Reached max attempts (" + maxSocketRetrys + ")");
+          return;
+        }
+        console.warn("Socket closed. Reconnecting...");
+        setTimeout(initSocket, 2000);
+      }
     }
-    fetchCss();
-    socket.on("connect", () => {
-      console.log("Connected!");
-    });
-    
-    socket.on("message", (msg: string) => {
-      if (msg === "refreshpage") {
-        window.location.reload();
-      }
-      if (msg === "refreshcss") {
-        fetchCss();
-      }
-    });
-  } else {
-    window.dev = false;
+    initSocket();
   }
 };
